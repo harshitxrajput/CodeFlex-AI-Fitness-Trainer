10ceeca91f05fb27be9e6e7938e957eaf97d50f0
harshitxrajput <harshitxrajput@gmail.com>
2025-05-12 16:22:32 +0530

    setup clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
 
 const HomePage = () => {
     return (
         <div>
             HomePage
+
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+
+            <SignedIn>
+                <SignOutButton />
+            </SignedIn>
         </div>
   )
 }
