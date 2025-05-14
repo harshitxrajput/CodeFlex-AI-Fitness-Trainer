f642f5fa16be4f825e508254f01047945bb0e5f5
harshitxrajput <harshitxrajput@gmail.com>
2025-05-14 23:49:34 +0530

    started ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
-
 const HomePage = () => {
     return (
-        <div>
-            HomePage
-
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-
-            <SignedIn>
-                <SignOutButton />
-            </SignedIn>
+        <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
+            <section className="relative z-10 py-24 flex-grow">
+                <div className="container mx-auto px-4">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center relative">
+                        {/* CORNER DECORATION */}
+                        
+                    </div>
+                </div>
+            </section>
         </div>
   )
 }
